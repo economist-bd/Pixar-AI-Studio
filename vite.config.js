46893406ea8d34d3c32e0324bde5f9bc7a545687
Md. Monjurul Haque <himel452@gmail.com>
2025-12-19 03:01:28 +0600

    Fix entry point and config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // আপনার রিপোজিটরির নাম অনুযায়ী base সেট করতে হবে
-  base: '/Pixar-AI-Studio/',
+  // ভার্সেলের জন্য বেস পাথ ঠিক রাখা জরুরি
+  base: '/',
 })
